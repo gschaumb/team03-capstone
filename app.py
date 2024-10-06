7d4e161a3fe3eda851cb37063fbfe126f3f9c8d3
Gary Schaumburg <gary@successmotions.com>
2024-10-06 09:29:34 -0700

    Testing flow from github to HF

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import gradio as gr
 
 def greet(name):
-    return "Hello " + name + "!!"
+    return "Damn good to meet you, " + name + "!!"
 
 demo = gr.Interface(fn=greet, inputs="text", outputs="text")
 demo.launch()
