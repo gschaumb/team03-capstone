7ee2ec0a66e27514d93c656716d9c29a90368a70
Gary Schaumburg <gschaumb@umich.edu>
2024-10-06 11:34:05 -0700

    more tests of action flow

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import gradio as gr
 
 def greet(name):
-    return "Danned good to meet you, Mr. " + name + "!!"
+    return "It is very nice to meet you, Dr. " + name + "!!"
 
 demo = gr.Interface(fn=greet, inputs="text", outputs="text")
 demo.launch()
