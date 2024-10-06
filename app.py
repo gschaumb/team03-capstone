4dd44cf206250d4532cd7ac24b73410a7a80f85b
Gary Schaumburg <gschaumb@umich.edu>
2024-10-06 12:08:46 -0700

    more debug of placeholder main.py

--- app.py
+++ app.py
@@ -4,10 +4,7 @@ from main import process_data  # Import function from main.py
 def create_interface():
     input_text = gr.Textbox(label="Input Text")
     output_text = gr.Textbox(label="Output Text")
-    
-    def process_text(input):
-        return input.upper()
 
-    return gr.Interface(fn=process_text, inputs=input_text, outputs=output_text)
+    return gr.Interface(fn=process_data, inputs=input_text, outputs=output_text)
     
 create_interface().launch()
