089e1bf34db431d851ea7f9295d356a6fef7db28
Gary Schaumburg <gary@successmotions.com>
2024-10-06 09:39:39 -0700

    action fail debugging

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import gradio as gr
 
 def greet(name):
-    return "Damn good to meet you, " + name + "!!"
+    return "Good to meet you, " + name + "!!"
 
 demo = gr.Interface(fn=greet, inputs="text", outputs="text")
 demo.launch()
